1966c68ce8fe320e1ea1ccebb6c7b80fb772b637
kareembujji <79572565+kareembujji@users.noreply.github.com>
2021-08-26 10:21:50 +0530

    Add files via upload

---
+++ matrices..py
@@ -0,0 +1,18 @@
+x=[[1,2,3],
+   [4,5,6],
+   [7,8,9]]
+y=[[0,1,2],
+   [3,4,5],
+   [6,7,8]]
+rssult=[[0,0,0],
+        [0,0,0],
+        [0,0,0]]
+#iterate through rows of x
+for i in range(len(x):
+               for j in range(len(y[0])):
+               for k in range (len(y)):
+               result[i][j] += x[i][k]*y[k][j]
+
+               
+for in range:
+print(r)
